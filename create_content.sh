56aea5b3c8773c2cbeed4e65855a657098ec546d
Nejc Bizjak <nb7232@student.uni-lj.si>
2016-04-13 09:15:18 -0700

    Refactored create_content.sh2

--- create_content.sh
+++ create_content.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+num_of_videos=100 #must be multiplier of 10
 content_size=5
-for (( filenum=1; filenum <= 10; filenum++ ))
+for (( filenum=1; filenum <= $num_of_videos; filenum++ ))
 do
     #Create file with specific content_size
     dd if=/dev/zero of=/home/ubuntu/mag/videos/video$filenum.mp4  bs=1M  count=$content_size
 
     case $filenum in
-        4 )
+        $((4*$num_of_videos/10)) )
             content_size=10 ;;
-        7 )
+        $((7*$num_of_videos/10)) )
             content_size=15 ;;
-        9 )
+        $((9*$num_of_videos/10)) )
             content_size=25 ;;
     esac
 
